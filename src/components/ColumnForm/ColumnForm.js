7c4a2b276c6c547adc24ccd12633362030b4ffe7
emilpio <emilpio@gmail.com>
2022-11-03 13:54:55 +0100

    add column to reducer

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,28 +1,40 @@
 import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
-import Button from '../Button/Button.js'
+import Button from '../Button/Button.js';
+import { useDispatch } from 'react-redux';
 
+const ColumnForm = (props) => {
+  const dispatch = useDispatch();
 
-
-const ColumnForm = props => {
-  
   const [title, setTitle] = useState('');
   const [icon, setIcon] = useState('');
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    props.action({ title: title, icon: icon });
+    dispatch({ type: 'ADD_COLUMN', payload: { title, icon } });
     setTitle('');
     setIcon('');
-}
+  };
 
-return (
-      <form className={styles.columnForm} onSubmit={handleSubmit}>
-          <span className={styles.span}>Title:</span><input className={styles.input} type="text" value={title}  onChange={e => setTitle(e.target.value)} />
-          <span className={styles.span}>Icon: </span> <input className={styles.input} type="text" value={icon} onChange={e => setIcon(e.target.value)} />
-                <Button>ADD COLUMN  </Button>
-      </form>
-);
-}
+  return (
+    <form className={styles.columnForm} onSubmit={handleSubmit}>
+      <span className={styles.span}>Title:</span>
+      <input
+        className={styles.input}
+        type='text'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <span className={styles.span}>Icon: </span>{' '}
+      <input
+        className={styles.input}
+        type='text'
+        value={icon}
+        onChange={(e) => setIcon(e.target.value)}
+      />
+      <Button>ADD COLUMN </Button>
+    </form>
+  );
+};
 
 export default ColumnForm;
